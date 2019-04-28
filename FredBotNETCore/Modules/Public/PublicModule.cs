1deb9bc914a93dc77ddf4e10375c5c3559e797b3
Matt <18367146+Stxtics@users.noreply.github.com>
2019-04-28 13:22:58 +0100

    add missing else statement

--- FredBotNETCore/Modules/Public/PublicModule.cs
+++ FredBotNETCore/Modules/Public/PublicModule.cs
@@ -745,7 +745,7 @@ namespace FredBotNETCore.Modules.Public
                                     $"Since they already have the bubble set, the prize was awarded to **{Format.Sanitize(Uri.UnescapeDataString(bubbles))}** instead!");
                             }
                         }
-                        if (finderID != 1)
+                        else if (finderID != 1)
                         {
                             if (Extensions.UserInGuild(null, guild, finderID.ToString()) != null)
                             {
